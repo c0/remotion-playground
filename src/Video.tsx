1d9b2e373b0c5091053385be351118520a6124dc
Caleb Wright <cwright@rentpath.com>
2021-06-25 14:29:13 -0400

    Eclipse

--- src/Video.tsx
+++ src/Video.tsx
@@ -2,6 +2,7 @@ import { Composition } from 'remotion'
 import { GooBall } from './GooBall'
 import { GooBallCSS } from './GooBallCSS'
 import { GooBallDrip } from './GooBallDrip'
+import { GooEclipse } from './GooEclipse'
 import { GooLine } from './GooLine'
 import { GooWoom } from './GooWoom'
 
@@ -29,6 +30,14 @@ export const RemotionVideo: React.FC = () => {
       <Composition
         id="GooBallDrip"
         component={GooBallDrip}
+        durationInFrames={15 * FPS}
+        fps={FPS}
+        width={500}
+        height={500}
+      />
+      <Composition
+        id="GooEclipse"
+        component={GooEclipse}
         durationInFrames={15 * FPS}
         fps={FPS}
         width={500}
