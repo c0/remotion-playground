e6c9a3aa05deed74692937a36a1c2f7dc2a450ee
Caleb Wright <cwright@rentpath.com>
2021-06-25 20:45:06 -0400

    Woom

--- src/Video.tsx
+++ src/Video.tsx
@@ -2,6 +2,7 @@ import { Composition } from 'remotion'
 import { GooBall } from './GooBall'
 import { GooBallCSS } from './GooBallCSS'
 import { GooBallDrip } from './GooBallDrip'
+import { GooWoom } from './GooWoom'
 
 const FPS = 30
 
@@ -27,6 +28,14 @@ export const RemotionVideo: React.FC = () => {
       <Composition
         id="GooBallDrip"
         component={GooBallDrip}
+        durationInFrames={15 * FPS}
+        fps={FPS}
+        width={500}
+        height={500}
+      />
+      <Composition
+        id="GooWoom"
+        component={GooWoom}
         durationInFrames={15 * FPS}
         fps={FPS}
         width={500}
